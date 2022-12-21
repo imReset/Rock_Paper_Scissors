36f826162ae2e49e75aab01a4a0f72e2c7af09e7
imReset <fffffreinds101@gmail.com>
2022-12-21 13:24:43 -0800

    add js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Rock Paper Scizors!");
+function getComputerChoice() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let randomChoice = Math.floor(Math.random() * choices.length);
+
+  //   console.log(choices[randomChoice]);
+}
+getComputerChoice();
+
+const playRound = function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+};
+
+function game() {
+  playRound();
+}
+
+game();
